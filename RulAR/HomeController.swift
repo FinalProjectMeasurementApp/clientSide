ea6a029f4ff3e8a81fc6c838cd6ae3927c9fdf86
violerine <viosefferine@gmail.com>
2018-07-27 12:23:22 +0700

    UI + http req

---
+++ RulAR/HomeController.swift
@@ -0,0 +1,60 @@
+//
+//  HomeController.swift
+//  RulAR
+//
+//  Created by Violerine on 26/07/18.
+//  Copyright © 2018 Michael Cangcianno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//struct Course{
+//    let id: Int
+//    let name: String
+//    let link: String
+//    let imageUrl: String
+//    let number_of_lessons: Int
+//}
+
+struct Shape: Decodable{
+    let sides: [Int]
+    let _id: String
+    let name: String
+    let area: Int
+    let perimeter: Int
+    let createdAt: String
+    let updatedAt: String
+    
+}
+
+class HomeController : UIViewController{
+    override func viewDidLoad() {
+        print("MUNCUL GA DISINI")
+        super .viewDidLoad()
+        let jsonUrl = "http://localhost:8000/shape/"
+        guard let url = URL(string: jsonUrl) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            
+            do{
+                print("masuk do ga")
+                let shape = try
+                    JSONDecoder().decode([Shape].self, from: data)
+                print("shape nameeeeee ",shape)
+
+            }catch let jsonErr {
+                print("Erroor",jsonErr)
+                print("response", response!)
+            }
+
+        }.resume()
+        
+        
+    }
+}
+
---
+++ RulAR/InputController.swift
@@ -0,0 +1,29 @@
+//
+//  InputController.swift
+//  RulAR
+//
+//  Created by Violerine on 26/07/18.
+//  Copyright © 2018 Michael Cangcianno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InputController : UIViewController{
+    
+    var textValue : String!
+    
+    @IBOutlet weak var nameInput: UITextField!
+//
+    @IBAction func inputChanged(_ sender: UITextField) {
+        print(sender.text!)
+        textValue = sender.text!
+    }
+    
+    
+    @IBAction func submitButton(_ sender: Any) {
+        print(textValue)
+        print("button di pencet")
+
+    }
+}
