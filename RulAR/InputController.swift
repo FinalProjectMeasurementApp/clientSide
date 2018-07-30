5b7f5ad76914507ff91faba29946735943e10391
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-07-30 09:06:31 +0700

    first commit

--- RulAR/InputController.swift
+++ RulAR/InputController.swift
@@ -11,6 +11,38 @@ import UIKit
 
 class InputController : UIViewController{
 
+    override func viewDidLoad() {
+//        let imageView = UIImageView(image: UIImage(named: "grid.png"))
+//        imageView.frame = view.bounds
+//        imageView.contentMode = UIViewContentMode.scaleAspectFill
+//        view.addSubview(imageView)
+//        
+//        let shape = CAShapeLayer()
+//        view.layer.addSublayer(shape)
+//        shape.opacity = 0.5
+//        shape.lineWidth = 2
+//        shape.lineJoin = kCALineJoinMiter
+//        shape.strokeColor = UIColor(hue: 0.786, saturation: 0.79, brightness: 0.53, alpha: 1.0).cgColor
+//        shape.fillColor = UIColor(hue: 0.786, saturation: 0.15, brightness: 0.89, alpha: 1.0).cgColor
+//        
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x: 120, y: 20))
+//        path.addLine(to: CGPoint(x: 230, y: 90))
+//        path.addLine(to: CGPoint(x: 240, y: 250))
+//        path.addLine(to: CGPoint(x: 40, y: 280))
+//        path.addLine(to: CGPoint(x: 100, y: 150))
+//        path.close()
+//        shape.path = path.cgPath
+//        
+//        
+//                            var myTextLayer = CATextLayer()
+//                            myTextLayer.string = "My text"
+//                            myTextLayer.foregroundColor = UIColor.black.cgColor
+//                                 myTextLayer.frame = imageView.bounds
+//        myTextLayer.position = CGPoint(x:200,y: 400)
+//                    print(myTextLayer.frame)
+//                            imageView.layer.addSublayer(myTextLayer)
+    }
     
     var textValue : String!
     
